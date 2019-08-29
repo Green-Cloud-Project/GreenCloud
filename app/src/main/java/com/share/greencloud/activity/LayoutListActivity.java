56e22b95bb6d7890d45e21d7480462fc278a9086
Bentley <p2c2kbf@gmail.com>
2019-08-29 18:15:54 +0900

    modified MapFragment class to get current location of user

--- app/src/main/java/com/share/greencloud/activity/LayoutListActivity.java
+++ app/src/main/java/com/share/greencloud/activity/LayoutListActivity.java
@@ -1,14 +1,20 @@
 package com.share.greencloud.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.share.greencloud.R;
 import com.share.greencloud.fragment.CameraFragment;
@@ -22,6 +28,8 @@ import com.share.greencloud.fragment.MapFragment;
 import com.share.greencloud.fragment.MyGreenFragment;
 import com.share.greencloud.googlelogin.GoogleLoginActivity;
 import com.share.greencloud.kakaologin.KakaoLoginActiviy;
+
+import timber.log.Timber;
 
 public class LayoutListActivity extends AppCompatActivity implements LayoutListFragment.CommunicateListener,
         GreenCloudInfoFragment.OnFragmentInteractionListener,
@@ -37,6 +45,9 @@ public class LayoutListActivity extends AppCompatActivity implements LayoutListF
 
     LoginFragment loginFragment;
 
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private boolean mLocationPermissionGranted;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +56,11 @@ public class LayoutListActivity extends AppCompatActivity implements LayoutListF
 
         vp = findViewById(R.id.vp);
         vp.setAdapter(new VpAdt(getSupportFragmentManager()));
+
+        if (!checkPermissions()) {
+            getLocationPermission();
+        }
+
     }
 
     @Override
@@ -138,5 +154,56 @@ public class LayoutListActivity extends AppCompatActivity implements LayoutListF
 
         if (loginFragment != null)
             loginFragment.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private boolean checkPermissions() {
+        int permissionState = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        return permissionState == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        Timber.d("onRequestPermissionsResult() is called");
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+                else {
+                    Toast.makeText(this, "위치정보 사용에 대한 동의가 거부되었습니다.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
---
+++ app/src/main/java/com/share/greencloud/lifecycleobserver/MyObserver.java
@@ -0,0 +1,31 @@
+package com.share.greencloud.lifecycleobserver;
+
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleObserver;
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.OnLifecycleEvent;
+import android.content.Context;
+
+import com.share.greencloud.kakaologin.GlobalApplication;
+
+import timber.log.Timber;
+
+public class MyObserver implements LifecycleObserver {
+    private final Context mContext;
+
+    public MyObserver(LifecycleOwner lifecycleOwner, Context context) {
+        mContext = context;
+        lifecycleOwner.getLifecycle().addObserver(this);
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    public void onResume() {
+        Timber.d("onResume is called");
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    public void onPause() {
+    }
+}
+
+
