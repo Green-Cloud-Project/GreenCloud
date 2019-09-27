a458a39e23310ae3d8a352d4ae9035b16f27cb43
Bentley <p2c2kbf@gmail.com>
2019-09-27 21:07:07 +0900

    display rental spots on map

---
+++ app/src/main/java/com/share/greencloud/model/RentalOffice.java
@@ -0,0 +1,50 @@
+package com.share.greencloud.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class RentalOffice {
+    @SerializedName("office_id")
+    private int office_id;
+    @SerializedName("office_name")
+    private String office_name;
+    private String office_des;
+    @SerializedName("lat")
+    private double lat;
+    @SerializedName("lon")
+    private double lon;
+    @SerializedName("umbrella_count")
+    private int umbrella_count;
+
+
+    public RentalOffice() {
+    }
+
+    public RentalOffice(String office_name) {
+        this.office_name = office_name;
+    }
+
+    public int getOffice_id() {
+        return office_id;
+    }
+
+    public String getOffice_name() {
+        return office_name;
+    }
+
+    public String getOffice_des() {
+        return office_des;
+    }
+
+    public double getLatitude() {
+        return lat;
+    }
+
+    public double getLongitude() {
+        return lon;
+    }
+
+    public int getUmbrella_count() {
+        return umbrella_count;
+    }
+}
+
