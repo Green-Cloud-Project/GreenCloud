451a2149fbe0e854876d2b0c2bb77989a39cae62
Bentley <p2c2kbf@gmail.com>
2019-11-18 19:34:40 +0900

    add ui of rental order confirm activity

---
+++ app/src/main/java/com/share/greencloud/presentation/activity/RentalOrderActivity.kt
@@ -0,0 +1,12 @@
+package com.share.greencloud.presentation.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.share.greencloud.R
+
+class RentalOrderActivity : AppCompatActivity(R.layout.activity_rental_order) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
