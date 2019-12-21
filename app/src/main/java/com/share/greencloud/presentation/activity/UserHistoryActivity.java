56dda2c3aec2384ef8d42ec5a7ae1f9394e3d60e
Bentley <p2c2kbf@gmail.com>
2019-12-21 12:29:27 +0900

    refectoring: remove unused files

--- app/src/main/java/com/share/greencloud/presentation/activity/UserHistoryActivity.java
+++ app/src/main/java/com/share/greencloud/presentation/activity/UserHistoryActivity.java
@@ -1,25 +1,15 @@
 package com.share.greencloud.presentation.activity;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
 import com.share.greencloud.R;
-import com.share.greencloud.common.BaseActivity;
-//import com.share.greencloud.databinding.ActivityUserHistoryBinding;
-import com.share.greencloud.presentation.adapter.AlarmRvAdt;
 import com.share.greencloud.presentation.adapter.UserHistoryPagerAdapter;
 
-//public class UserHistoryActivity extends BaseActivity<ActivityUserHistoryBinding> {
-
 public class UserHistoryActivity extends AppCompatActivity {
-
-    //private ActivityUserHistoryBinding binding;
-    //private AlarmRvAdt adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,61 +23,5 @@ public class UserHistoryActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-
-        /*
-        binding = getBinding();
-        setupToolbar();
-        setuoView();
-        loadData();
-         */
     }
-
-    /*
-    @Override
-    public int getLayoutResId() {
-        return R.layout.activity_user_history;
-    }
-
-    private void setupToolbar() {
-        setSupportActionBar(binding.toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-    }
-
-    public void setuoView() {
-        binding.contentUserHistory.userHistoryRecyclerView.setHasFixedSize(true);
-
-        binding.contentUserHistory.userHistorySwipeLayout.setOnRefreshListener(() -> {
-            new Handler().postDelayed(() -> {
-                loadData();
-                binding.contentUserHistory.userHistorySwipeLayout.setRefreshing(false);
-            }, 1000);
-
-        });
-        binding.contentUserHistory.userHistorySwipeLayout.setColorSchemeResources(
-                android.R.color.holo_blue_bright,
-                android.R.color.holo_green_light
-        );
-    }
-
-    public void loadData() {
-        //todo replace with rest API later..
-        if (adapter == null) {
-            adapter = new AlarmRvAdt();
-        }
-        binding.contentUserHistory.userHistoryRecyclerView.setAdapter(adapter);
-        adapter.notifyDataSetChanged();
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home: { //toolbar의 back키 눌렀을 때 동작
-                finish();
-                return true;
-            }
-        }
-        return super.onOptionsItemSelected(item);
-    }
-     */
 }
--- app/src/main/java/com/share/greencloud/presentation/fragment/OnBoardingFragment.java
+++ app/src/main/java/com/share/greencloud/presentation/fragment/OnBoardingFragment.java
@@ -1,18 +1,17 @@
 package com.share.greencloud.presentation.fragment;
 
-import androidx.lifecycle.ViewModelProviders;
-
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
 import com.share.greencloud.R;
+import com.share.greencloud.presentation.viewmodel.OnBoardingViewModel;
 
 public class OnBoardingFragment extends Fragment {
 
